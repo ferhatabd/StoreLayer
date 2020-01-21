b299a3028eb03187780e37ea561e0163e9cd3127
Ferhat Abdullahoglu <abdullahoglu@sabanciuniv.edu>
2020-01-21 21:29:19 +0300

    Test case updates

--- Tests/StoreLayerTests/StoreLayerTests.swift
+++ Tests/StoreLayerTests/StoreLayerTests.swift
@@ -6,7 +6,7 @@ final class StoreLayerTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(StoreLayer().text, "Hello, World!")
+       // XCTAssertEqual(IAPStore().text, "Hello, World!")
     }
 
     static var allTests = [
